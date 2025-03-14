c93f3861598ffbe412b228e2bce9795fdce375b3
Montagvonsontag <37384660+Montagvonsontag@users.noreply.github.com>
2025-03-14 14:14:10 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 import "bootstrap";
 import "./style.css";
 
+ 
+let first= ['herring', 'Serrano Ham', 'smoked salmon', 'snails'];
+let second = ['strawberry jam', 'Nocilla', 'Marmite', 'honey', 'castor oil'];
+let third= ['watermelon', 'banana', 'durian fruit', 'rotten tomatoes'];
+let fourth= ['salt', 'paprika', 'cinamon', 'coriander', 'sugar'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//generates random numbers
+function randomGenerator(array){
+  let randomPosition;
+  randomPosition= array[Math.floor(Math.random()*array.length)];
+  return randomPosition;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function ingredientGenerator(first, second, third, fourth) {
+  let ingredient= "";
+  ingredient= randomGenerator(first)+ ", "+ randomGenerator(second)+ ", " + randomGenerator(third) + " and " + randomGenerator(fourth);
+  return ingredient;
+}
+
+
+let recipe=ingredientGenerator(first, second, third, fourth);
+document.getElementById("recipe").innerHTML = recipe;
