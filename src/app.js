d745a268284783d580f1ac90b9103505bce0e039
Montagvonsontag <37384660+Montagvonsontag@users.noreply.github.com>
2025-03-18 11:23:18 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,24 +2,24 @@ import "bootstrap";
 import "./style.css";
 
  
-let first= ['Herring', 'Serrano ham', 'Smoked salmon', 'Snails'];
-let second = ['strawberry jam', 'Nocilla', 'Marmite', 'honey', 'castor oil'];
-let third= ['watermelon', 'banana', 'durian fruit', 'rotten tomatoes'];
-let fourth= ['salt', 'paprika', 'cinamon', 'coriander', 'sugar'];
+let firstIngredient= ['Herring', 'Serrano ham', 'Smoked salmon', 'Snails'];
+let secondIngredient = ['strawberry jam', 'Nocilla', 'Marmite', 'honey', 'castor oil'];
+let thirdIngredient= ['watermelon', 'banana', 'durian fruit', 'rotten tomatoes'];
+let fourthIngredient= ['salt', 'paprika', 'cinamon', 'coriander', 'sugar'];
 
-//generates random numbers
+//This function generates random numbers within the array's lenght
 function randomGenerator(array){
   let randomPosition;
   randomPosition= array[Math.floor(Math.random()*array.length)];
   return randomPosition;
 }
-
-function ingredientGenerator(first, second, third, fourth) {
-  let ingredient= "";
-  ingredient= randomGenerator(first)+ ", "+ randomGenerator(second)+ ", " + randomGenerator(third) + " and " + randomGenerator(fourth);
-  return ingredient;
+//This function gets the ingredients from the four arrays which correspond to the random positions generated
+function ingredientGenerator(firstIngredient, secondIngredient, thirdIngredient, fourthIngredient) {
+  let ingredients= "";
+  ingredients= randomGenerator(firstIngredient)+ ", "+ randomGenerator(secondIngredient)+ ", " + randomGenerator(thirdIngredient) + " and " + randomGenerator(fourthIngredient);
+  return ingredients;
 }
 
 
-let recipe=ingredientGenerator(first, second, third, fourth);
+let recipe=ingredientGenerator(firstIngredient, secondIngredient, thirdIngredient, fourthIngredient);
 document.getElementById("recipe").innerHTML = recipe;
